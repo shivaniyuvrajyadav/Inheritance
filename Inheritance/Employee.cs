ee7122b134181fe96bdab6548311e3174cffb75c
Admin <Admin@LAPTOP-V3GL5398>
2024-08-06 13:23:04 +0530

    .

---
+++ Inheritance/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public  class Employee
+    { 
+        public int Experience { get; set; }
+
+        public void Calculatesalary()
+        {
+            int salary = Experience * 30000;
+            Console.WriteLine("salary : {0}", salary);
+        }
+
+    }
+
+    public class permanentemployee : Employee
+    {
+        static void Main (string[] args)
+        {
+            permanentemployee p = new permanentemployee();
+            p.Experience = 5;
+            p.Calculatesalary();
+
+            Console.ReadLine();
+        }
+    }
+
+}
+
+/* what is inheritance ? when to use inheritance ?
+ *  inheritance is a parent - child relationship between two classes , where child
+ * class will automatically get the properties and methods of the parent .  
+ * 
+ *  inheritance is good for reusability and abstraction of code. 
+ * 
+ */
